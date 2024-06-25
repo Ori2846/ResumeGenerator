80e22477e08ed80267195c53774973f0d4a576d7
Hendrick <hdo2846@gmail.com>
2024-06-24 17:42:10 -0700

    Node v20.15.0

--- NextJS-App/resgen/pages/index.js
+++ NextJS-App/resgen/pages/index.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import 'tailwindcss/tailwind.css';
 import Footer from '/components/Footer';
-import '../polyfills.mjs'; 
+
 
 export default function Home() {
   const router = useRouter();
